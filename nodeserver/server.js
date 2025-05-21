7a0ab7728fdfb0d103d8425148c181594dee79a7
Kannika KR <165569310+Kannikakr@users.noreply.github.com>
2025-05-21 16:26:43 +0530

    Update server.js

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -1,33 +1,37 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
-const {createServer} = require("http");
-const {Server} = require("socket.io");
+const port = process.env.PORT || 8000;
 const httpServer = createServer();
-const io = new Server(httpServer,{
-    cors : {
-        origin : "*",
-        methods : ["GET", "POST"]
-    }
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
-//const io = require("socket.io")(8000);
+
 const users = {};
 
- io.on('connection', socket=>{
-     socket.on('new-user-joined', name=>{
-         console.log("New user", name);
-         users[socket.id] = name;
-         socket.broadcast.emit('user-joined', name);
-     });
+io.on('connection', socket => {
+  socket.on('new-user-joined', name => {
+    console.log("New user:", name);
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  });
 
-     socket.on('send', message=>{
-         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
-     });
-
-     socket.on('disconnect', message=>{
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
+  socket.on('send', message => {
+    socket.broadcast.emit('receive', {
+      message: message,
+      name: users[socket.id]
     });
- })
+  });
 
- httpServer.listen(8000,()=>{
-    console.log('server is listening on port 8000');
- });
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('left', users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+httpServer.listen(port, '0.0.0.0', () => {
+  console.log(`Server is listening on port ${port}`);
+});
