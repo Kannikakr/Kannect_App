3b67da8e8ddd4a3463fb08910f87022b6aaf2a38
kannikakr <kannikashivu18@gmail.com>
2025-05-21 15:04:16 +0530

    added my chat app

---
+++ nodeserver/index.js
@@ -0,0 +1,37 @@
+// This is Node server. this will handle socket io connections
+
+// io.o, edu yalla users yar yar join agta ro avrna yalla listen madute.
+//socket.io for the perticular user
+// node server which will handle socket io connections
+const {createServer} = require("http");
+const {Server} = require("socket.io");
+const httpServer = createServer();
+const io = new Server(httpServer,{
+    cors : {
+        origin : "*",
+        methods : ["GET", "POST"]
+    }
+});
+//const io = require("socket.io")(8000);
+const users = {};
+
+ io.on('connection', socket=>{
+     socket.on('new-user-joined', name=>{
+         console.log("New user", name);
+         users[socket.id] = name;
+         socket.broadcast.emit('user-joined', name);
+     });
+
+     socket.on('send', message=>{
+         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+     });
+
+     socket.on('disconnect', message=>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+ })
+
+ httpServer.listen(8000,()=>{
+    console.log('server is listening on port 80000');
+ });
